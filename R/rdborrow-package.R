1bb279c6b6859881079e662f1256058bdbae29bb
LeiShi-rocks <leishi1998@gmail.com>
2024-04-20 11:53:20 -0700

    update the instruction file

--- R/rdborrow-package.R
+++ R/rdborrow-package.R
@@ -11,7 +11,21 @@
 #' @section Authors:
 #' The following authors contribute to the development and maintainance of the package:
 #' 
+#' - Lei Shi, University of California Berkeley, [HOMEPAGE](https://leishi-rocks.github.io/index.html), [leishi1998@gmail.com](mailto:leishi1998@gmail.com)
+#' 
+#' - Herbert Pang, Genentech Inc.
+#' 
+#' - Chen Chen, Genentech Inc.
+#' 
+#' - Jiawen Zhu, Genentech Inc.
+#' 
 #' @section See Also: 
+#' 
 #' Useful links:
+#' 
+#' - [GitHub Repo for rdborrow](https://github.com/pathwayrf/rdborrow)
+#' 
+#' - [Estimating treatment effect in randomized trial after control to treatment crossover using external controls](https://www.tandfonline.com/doi/full/10.1080/10543406.2024.2330209)
+#' 
 #' 
 NULL
