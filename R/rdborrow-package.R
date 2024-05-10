c1feec0adcc3c397a8da28bb70756ce542669727
LeiShi-rocks <leishi1998@gmail.com>
2024-05-10 11:45:48 -0700

    update author info

--- R/rdborrow-package.R
+++ R/rdborrow-package.R
@@ -11,9 +11,9 @@
 #' @section Authors:
 #' The following authors contribute to the development and maintainance of the package:
 #' 
-#' - Lei Shi, University of California Berkeley, \href{mailto:leishi1998@gmail.com}{leishi@berkeley.edu}
+#' - Lei Shi, University of California Berkeley, \href{mailto:leishi1998@gmail.com}{leishi1998@gmail.com}
 #' 
-#' - Herbert Pang, Genentech Inc.
+#' - Herbert Pang, Genentech Inc. \href{mailto:pathwayrf@gmail.com}{pathwayrf@gmail.com}
 #' 
 #' - Chen Chen, Genentech Inc.
 #' 
@@ -27,5 +27,5 @@
 #' 
 #' - Estimating treatment effect in randomized trial after control to treatment crossover using external controls: \url{https://www.tandfonline.com/doi/full/10.1080/10543406.2024.2330209}
 #' 
-#' 
+#' - Causal estimators for incorporating external controls in randomized trials with longitudinal outcomes. In submission.
 NULL
